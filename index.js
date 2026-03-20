5cdc90dae84c2db27e4c7516b2a75c73661a131b
Kilian <dankestmirror@hotmail.es>
2026-03-20 16:18:34 +0000

    1 iteration

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'John';
+let hacker2 = 'Kilian';
 
-
+console.log(`The driver's is ${hacker1}
+The navegator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
